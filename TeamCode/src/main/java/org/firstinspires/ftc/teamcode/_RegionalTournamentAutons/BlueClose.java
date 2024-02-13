983ad0e53d6695d4a90239316554dcf0354e868d
Anirudh Narayanan <109127469+ripark2@users.noreply.github.com>
2024-02-12 19:46:49 -0600

    Update BlueClose.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_RegionalTournamentAutons/BlueClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_RegionalTournamentAutons/BlueClose.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode._RegionalTournamentAutons;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -18,18 +19,38 @@ public class BlueClose extends LinearOpMode {
 
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        Pose2d startPose = new Pose2d(12, 60, Math.toRadians(0));
+        Pose2d startPose = new Pose2d(12, 60, Math.toRadians(90));
 
+        drive.setPoseEstimate(startPose);
 
-        TrajectorySequence leftPose = drive.trajectorySequenceBuilder(startPose)
-                .lineToLinearHeading(new Pose2d(12,58, Math.toRadians(0)))
+        TrajectorySequence test = drive.trajectorySequenceBuilder(startPose)
+
+                .lineToLinearHeading(new Pose2d(15,34,Math.toRadians(-10)))
+                .waitSeconds(.5)
+                .splineToSplineHeading(new Pose2d(47,23,Math.toRadians(180)), Math.toRadians(-90))
+                .waitSeconds(1)
+                .splineToConstantHeading(new Vector2d(38,12), Math.toRadians(-90))
+                .lineToConstantHeading(new Vector2d(-53,12))
+                .waitSeconds(2)
+
+                .lineToConstantHeading(new Vector2d(38,12))
+                .splineToConstantHeading(new Vector2d(48,30), Math.toRadians(90))
+
+                .waitSeconds(2)
+
+                .splineToConstantHeading(new Vector2d(38,12), Math.toRadians(-90))
+                .lineToConstantHeading(new Vector2d(-53,12))
+                .waitSeconds(2)
+
+                .lineToConstantHeading(new Vector2d(38,12))
+                .splineToConstantHeading(new Vector2d(48,30), Math.toRadians(90))
                 .build();
 
         waitForStart();
 
 
 
-        drive.followTrajectorySequence(leftPose);
+        drive.followTrajectorySequence(test);
 
         while (!isStopRequested() && opModeIsActive());
 
